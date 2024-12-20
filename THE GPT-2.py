d55c254022cbcfe0030b1ceb951957998533d1d4
Devashish tyagi <144609436+Devashish262@users.noreply.github.com>
2024-12-20 11:28:48 +0530

    Update THE GPT-2.py

--- THE GPT-2.py
+++ THE GPT-2.py
@@ -62,3 +62,14 @@ class LayerNorm(nn.Module):
         self.eps=1e-5
         self.scale=nn.Parameter(torch.ones(emb_dim))
         self.shift=nn.Parameter(torch.zeros(emb_dim))
+    def forward(self,x):
+        mean=x.mean(dim=-1,keepdim=True)
+        var=x.var(dim=-1,keepdim=True,unbiased=False)
+        norm_x=(x-mean)/torch.sqrt(var+self.eps)
+        return self.scale*norm_x+self.shift
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self,x):
+        return 0.5*x*(1+torch.tanh(torch.sqrt(torch.tensor(2.9/torch.pi))*(x+0.044715*torch.pow(x,3))))
